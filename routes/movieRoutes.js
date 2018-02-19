c57a9e64cee457eaf8b4625dfd48d48ec1252746
Sebastian Uddén <sebastian.udden@eurostep.com>
2018-02-19 14:38:27 +0100

    Updated movieroutes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -38,30 +38,24 @@ var routes = function(Movie) {
             res.json(req.movie);
         })
         .put(function(req, res) {
-            console.log('+++ req +++');
-            console.log(req);
-            console.log('+++++++++++');
-            req.movie.title = req.body.title || req.movie.title;
-            req.movie.director = req.body.director || req.movie.director;
-            req.movie.genre = req.body.genre || req.movie.genre;
-            req.movie.watched = req.body.watched || req.movie.watched;
+            if (req.body._id) {
+                delete req.body._id;
+            }
+            for (let property in req.body) {
+                req.movie[property] = req.body[property] || req.movie[property];
+            }
             req.movie.save();
             res.json(req.movie);
         })
         .patch(function(req, res) {
-            console.log('+++ req +++');
-            console.log(req);
-            console.log('+++++++++++');
             if (req.body._id) {
                 delete req.body._id;
             }
-            console.log('+++ req.body +++');
-            console.log(req.body);
-            console.log('++++++++++++++++');
             for (let property in req.body) {
-                req.movie[property] = req.body[property];
+                req.movie[property] = req.body[property] || req.movie[property];
             }
             req.movie.save();
+            res.json(req.movie);
         });
     return movieRouter;
 };
